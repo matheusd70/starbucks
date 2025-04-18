48257374acfbef3d945684abfab1e42755112f60
matheusd70 <matheusdias578@gmail.com>
2025-04-18 12:01:12 -0300

    Primeiro commit

---
+++ scripts.js
@@ -0,0 +1,14 @@
+
+
+let imagem = document.querySelector(".imagem-copo")
+let circulo = document.querySelector(".circulo")
+
+function trocaImagem(endereco) {
+    imagem.src = endereco
+
+}
+
+function trocaCor(cor) {
+   circulo.style.background = cor
+
+}
